8027ff9a8fdd141277567a9553f90a736926710f
sean-sanchez02 <sean.sanchez02@utrgv.edu>
2025-02-03 18:47:55 +0000

    definitions

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -9,7 +9,7 @@ public class MixMatch {
         boolean green = true;
         double pi = 3.14159;
 
-        System.out.println("Random integer: " + number + "  Random String: " + name + "  Random char: " + grade + "  Random short: " + max + "  Random long: " + min + "  Random boolean: " + green + "  Random double: " + pi + "  Random float: " + decimals);
+        System.out.println("integer, a value between -2^31 and 2^(31)-1 : " + number + "  String, a random word/words: " + name + "  char, a character or ascii value: " + grade + "  short, a value between -32,768 and +32,767: " + max + "  long, a value between -2^63 and 2^(63) -1: " + min + "  boolean, only true or false: " + green + "  double, a floating point between +-1.7 * 10^-308 and +-1.7 * 10^308, and can have 15 decimal places : " + pi + "  float, a floating point between +-3.4 8 10^-38 and +-3.4 * 10^38, and has 7 decimal places: " + decimals);
 
     }
 }
