e308ceaf55a59286ea909ba3742751972908b6a4
sean-sanchez02 <sean.sanchez02@utrgv.edu>
2025-02-03 04:37:33 +0000

    mixing

---
+++ src/MixMatch.java
@@ -0,0 +1,15 @@
+public class MixMatch {
+    public static void main(String[] arg) {
+        int number = 7;
+        String name = "size";
+        char grade = 'A';
+        short max = 32000;
+        long min = -500000000;
+        float decimals = 7.3f;
+        boolean green = true;
+        double pi = 3.14159;
+
+        System.out.println("Random integer: " + number + "  Random String: " + name + "  Random char: " + grade + "  Random short: " + max + "  Random long: " + min + "  Random boolean: " + green + "  Random double: " + pi + "  Random float: " + decimals);
+
+    }
+}
